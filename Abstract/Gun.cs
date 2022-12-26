150bd67037537aebc791cf4422e64a77b0eb96a8
Isakhanyan Nikolay <isaxanyan23@outlook.com>
2022-12-26 11:00:41 +0400

    Add project files.

---
+++ Abstract/Gun.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Gun : Weapon
+    {
+        public override void Fire()
+        {
+            Console.WriteLine("Granat");
+        }
+    }
+    class LaserGun : Weapon
+    {
+        public override void Fire()
+        {
+            Console.WriteLine("Lazer");
+        }
+    }
+}
---
+++ Abstract/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Abstract;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Player player = new Player();
+        Weapon[] _players = { new LaserGun(), new Gun() };
+        foreach (Weapon weap in _players)
+        {
+            player.Fire(weap);
+            Console.WriteLine();
+        }
+    }
+}
